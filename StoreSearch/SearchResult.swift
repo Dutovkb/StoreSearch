57dce173a5c1429b28b75c6f4adaa30549bf8767
Kirill Dutov <dutovapple@yandex.ru>
2021-03-12 09:47:44 +0300

    Add SearchBar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Кирилл Дутов on 12.03.2021.
+//
+
+import UIKit
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -8,12 +8,90 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    var searchResults = [SearchResult]()
+    var hasSearched = false
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchResults = []
+        
+        if searchBar.text! != "justin bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake Result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+            hasSearched = true
+            tableView.reloadData()
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel?.text = searchResults[indexPath.row].name
+            cell.detailTextLabel?.text = searchResults[indexPath.row].artistName
+        }
+        
+        return cell
+    }
+    
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+    //Снимает выделение строки с анимацеий
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // Гарантирует, что можно выбирать строки только с реальными результатами поиска
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
